192e8d5098c664115dfd63658e8f6b974c3ea19f
Soundwave <lihang00@gmail.com>
2014-09-19 00:09:46 -0700

    cal stats

--- criteo/src/utility.py
+++ criteo/src/utility.py
@@ -8,6 +8,38 @@ def log_feature_cnt(X_trn, X_tst):
     if X_trn.shape[1] != X_tst.shape[1]:
         logger.info('WARNING, feature cnt does not match!!!!')
 
+def get_categorical_stat(trn_feature, tst_feature, trn_label, trn_idx):
+    """Stats on categorical features"""
+    
+    smooth_up = 1
+    smooth_down = 3
+    local_stats = get_stat(trn_feature, trn_label, smooth_up, smooth_down, trn_idx)
+    trn_cat_stats = trn_feature.apply(lambda x: get_pr(local_stats, x, smooth_up, smooth_down))
+
+    trn_stats = get_stat(trn_feature, trn_label, smooth_up, smooth_down)
+    tst_cat_stats = tst_feature.apply(lambda x: get_pr(trn_stats, x, smooth_up, smooth_down))
+    return np.matrix(trn_cat_stats).reshape(len(trn_feature), 1), np.matrix(tst_cat_stats).reshape(len(tst_feature), 1)
+
+def get_pr(stats, cat, smooth_up, smooth_down):
+    pos_total = stats.get(cat, [smooth_up, smooth_down])
+    return pos_total[0] / float(pos_total[1])
+    
+def get_stat(features, labels, smooth_up, smooth_down, idx=[]):
+    stats = {}
+
+    if len(idx)==0:
+        idx=range(len(features))
+
+    for i in idx:
+        key = features[i]
+        label = labels[i]
+        if key not in stats:
+                stats[key] = [smooth_up, smooth_down] # count of true, total
+            stats[key][1] = stats[key][1] + 1
+            if label == True or label == 1:
+                stats[key][0] = stats[key][0] + 1
+    return stats
+
 def encode_categorical_feature(trn_feature, tst_feature=[], min_obs=10, n=None):
     """Encode the Pandas column into sparse matrix with one-hot-encoding."""
 
